ad9789021c4be35c69391c2df31a0723fa15e329
Harshitha <reddyharshitha246@gmail.com>
2025-10-16 22:44:11 +0530

    final project1

--- SMARTDOC NEW/src/main.jsx
+++ SMARTDOC NEW/src/main.jsx
@@ -1,6 +1,5 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
 // import { GoogleOAuthProvider } from '@react-oauth/google';
 // import { AuthProvider } from './context/AuthContext';
 // import App from './App.jsx';
@@ -9,18 +8,15 @@
 // ReactDOM.createRoot(document.getElementById('root')).render(
 //   <React.StrictMode>
 //     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-//       <BrowserRouter>
-//         <AuthProvider>
-//           <App />
-//         </AuthProvider>
-//       </BrowserRouter>
+//       <AuthProvider>
+//         <App />
+//       </AuthProvider>
 //     </GoogleOAuthProvider>
 //   </React.StrictMode>
 // );
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthProvider } from './context/AuthContext';
 import App from './App.jsx';
@@ -29,11 +25,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </GoogleOAuthProvider>
   </React.StrictMode>,
 );
